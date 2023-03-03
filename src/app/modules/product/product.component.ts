af43cd655b0f938c121c71ce56578406f100effd
Adrian Zawadzki <zawadzkii@tlen.pl>
2023-03-03 13:05:34 +0100

    add pagination

--- src/app/modules/product/product.component.ts
+++ src/app/modules/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Product } from './model/product';
 import { ProductService } from './product.service';
+import { Page } from 'src/app/shared/model/page';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-product',
@@ -9,19 +11,24 @@ import { ProductService } from './product.service';
 })
 export class ProductComponent {
 
-  products: Product[] = [];
+  page!: Page<Product>;
 
-
-  constructor(private productService: ProductService) {
-
-  }
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.getProducts();
+    this.getProducts();  
   }
 
   getProducts() {
-   this.productService.getProducts()
-    .subscribe(products => this.products = products);
+    this.getProductPage(0, 10);
+  }
+  
+  onPageEvent(event: PageEvent){
+    this.getProductPage(event.pageIndex, event.pageSize);
+  }
+  
+  private getProductPage(page: number, size: number) {
+    this.productService.getProducts(page, size)
+      .subscribe(page => this.page = page);
   }
 }
--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from './model/product';
+import { Page } from 'src/app/shared/model/page';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,30 +12,7 @@ export class ProductService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>("/api/products");
-    // return [
-    //   {
-    //     name: "p1",
-    //     category: "k1",
-    //     description: "opis ",
-    //     price: 11.99,
-    //     currency: "PLN"
-    //   },
-    //   {
-    //     name: "p12",
-    //     category: "k21",
-    //     description: "opis 2",
-    //     price: 11.99,
-    //     currency: "PLN"
-    //   },
-    //   {
-    //     name: "p13",
-    //     category: "k13",
-    //     description: "opis3 ",
-    //     price: 11.99,
-    //     currency: "PLN"
-    //   }
-    // ]
+  getProducts(page: number, size: number): Observable<Page<Product>> {
+    return this.http.get<Page<Product>>(`/api/products?page=${page}&size=${size}`);
   }
 }
